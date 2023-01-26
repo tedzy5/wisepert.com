986cb015611c1a1c97048a1fed9537d91434e9fe
Theodor Abi-Mansour <theodor@abimansour.com>
2023-01-26 23:21:07 +0200

    wisepert.com Laravel update files

---
+++ resources/views/form.blade.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,700,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Style Wisepert -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Wise Expert - Wisepert</title>
+</head>
+<body>
+
+
+<div class="content">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mr-auto">
+                <div class="m-0 mb-5 align-items-center text-center">
+                    <img src="./images/wisepert-dark-logo.jpg" class="w-50 m-2 mb-3" />
+                    <p class="text-white text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus blanditiis, perferendis aliquam.</p>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="text-white h5 mb-3">Beirut, Lebanon</h3>
+                        <ul class="list-unstyled mb-5">
+                            <li class="d-flex text-white mb-2">
+                                <span class="mr-3"><span class="icon-map"></span></span> 34 Street Name, Beirut, Lebanon
+                            </li>
+                            <li class="d-flex text-white mb-2"><span class="mr-3"><span class="icon-phone"></span></span> +961 (76) 123 456</li>
+                            <li class="d-flex text-white"><span class="mr-3"><span class="icon-envelope-o"></span></span> info@wisepert.com </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <h3 class="text-white h5 mb-3">London, United Kingdom</h3>
+                        <ul class="list-unstyled mb-5">
+                            <li class="d-flex text-white mb-2">
+                                <span class="mr-3"><span class="icon-map"></span></span> 34 Street Name, London, United Kingdom
+                            </li>
+                            <li class="d-flex text-white mb-2"><span class="mr-3"><span class="icon-phone"></span></span> +45 (123) 456 7890</li>
+                            <li class="d-flex text-white"><span class="mr-3"><span class="icon-envelope-o"></span></span> info@wisepert.com </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="box">
+                    <h3 class="heading">Help us provide you your best</h3>
+                    <form class="mb-5" method="post" id="contactForm" name="contactForm">
+                        <div class="row">
+
+                            <div class="col-md-12 form-group">
+                                <label for="name" class="col-form-label">Name</label>
+                                <input type="text" class="form-control" name="name" id="name">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group">
+                                <label for="email" class="col-form-label">Email</label>
+                                <input type="text" class="form-control" name="email" id="email">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-12 form-group">
+                                <label for="message" class="col-form-label">Message</label>
+                                <textarea class="form-control" name="message" id="message" cols="30" rows="7"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <input type="submit" value="Send Message" class="btn btn-block btn-info rounded-0 py-2 px-4">
+                                <span class="submitting"></span>
+                            </div>
+                        </div>
+                    </form>
+
+                    <div id="form-message-warning mt-4"></div>
+                    <div id="form-message-success">
+                        Your message was sent, thank you!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
